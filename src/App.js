863b84624bdb6c421050f0e9964d8522b4e40d44
YouriDK <youri.choucoutou@gmail.com>
2021-03-02 20:49:39 +0100

    [UPDATE ]Loading

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ import Chat from "./Components/Chat";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "./Components/Login";
 import { auth } from "./firebase";
+import Spinner from "react-spinkit";
 
 function App() {
-  const [user, loadin] = useAuthState(auth);
-
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://www.esecad.com/wp-content/uploads/sites/38/2016/11/slack-chat.png"
+            alt=""
+          />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
   return (
     <div className="App">
       <Router>
@@ -41,4 +54,26 @@ export default App;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `;
